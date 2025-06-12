62e203090adb6343d63aea74ce54623248ae56c5
nahhhu <nahucordero06@gmail.com>
2025-06-12 23:05:24 +0000

    Prueba de post y get

---
+++ demo/src/main/java/com/techlab/spring/TechLabSpringApplication.java
@@ -0,0 +1,14 @@
+package com.techlab.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechLabSpringApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(TechLabSpringApplication.class, args);
+	}
+
+}
