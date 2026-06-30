c0a96a5ddf649cd928365852ec15edec52b9d067
nahhhu <nahucordero06@gmail.com>
2026-06-30 18:38:53 -0300

    Se agrego busqueda por estado en PedidoRepository

--- demo/src/main/java/com/techlab/spring/repository/PedidoRepository.java
+++ demo/src/main/java/com/techlab/spring/repository/PedidoRepository.java
@@ -1,8 +1,11 @@
 package com.techlab.spring.repository;
 
+import com.techlab.spring.entity.EstadoPedido;
 import com.techlab.spring.entity.Pedido;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
+import java.util.List;
 
+public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
+List<Pedido> findByEstado(EstadoPedido estado);
 }
